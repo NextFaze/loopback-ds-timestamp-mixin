c9dc00ec01069841ff77d78e3cdf089a701b29be
Zak Barbuto <zbarbuto@nextfaze.com>
2016-07-22 12:00:51 +0930

    Add transpiled files
    Not pushing to npm at this stage so need these to be committed for the package to work.

---
+++ time-stamp.js
@@ -0,0 +1,81 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+var _debug2 = require('./debug');
+
+var _debug3 = _interopRequireDefault(_debug2);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var debug = (0, _debug3.default)();
+var warn = function warn(options, message) {
+  if (!options.silenceWarnings) {
+    console.warn(message);
+  }
+};
+var types = {
+  unix: 'number',
+  date: Date
+};
+
+exports.default = function (Model) {
+  var bootOptions = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1];
+
+  debug('TimeStamp mixin for Model %s', Model.modelName);
+
+  var options = _extends({
+    createdAt: 'createdAt',
+    updatedAt: 'updatedAt',
+    required: true,
+    type: 'date',
+    validateUpsert: false, // default to turning validation off
+    silenceWarnings: false
+  }, bootOptions);
+  options.type = types[options.type];
+
+  debug('options', options);
+
+  if (!options.validateUpsert && Model.settings.validateUpsert) {
+    Model.settings.validateUpsert = false;
+    warn(options, Model.pluralModelName + ' settings.validateUpsert was overriden to false');
+  }
+
+  if (Model.settings.validateUpsert && options.required) {
+    warn(options, 'Upserts for ' + Model.pluralModelName + ' will fail when\n          validation is turned on and time stamps are required');
+  }
+
+  Model.defineProperty(options.createdAt, {
+    type: options.type,
+    required: options.required,
+    defaultFn: options.type === types.unix ? undefined : 'now',
+    default: options.type === types.unix ? Date.now : undefined
+  });
+
+  Model.defineProperty(options.updatedAt, {
+    type: options.type,
+    required: options.required
+  });
+
+  Model.observe('before save', function (ctx, next) {
+    debug('ctx.options', ctx.options);
+    if (ctx.options && ctx.options.skipUpdatedAt) {
+      return next();
+    }
+    if (ctx.instance) {
+      debug('%s.%s before save: %s', ctx.Model.modelName, options.updatedAt, ctx.instance.id);
+      ctx.instance[options.updatedAt] = options.type === types.unix ? Date.now() : new Date();
+    } else {
+      debug('%s.%s before update matching %j', ctx.Model.pluralModelName, options.updatedAt, ctx.where);
+      ctx.data[options.updatedAt] = options.type === types.unix ? Date.now() : new Date();
+    }
+    return next();
+  });
+};
+
+module.exports = exports.default;
+//# sourceMappingURL=time-stamp.js.map
